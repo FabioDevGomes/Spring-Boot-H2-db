ddceebb7b106d6d15f6e26d03b9ad136c71201b5
Fábio <fabio.dev.gomes@gmail.com>
2016-12-27 12:56:40 -0200

    Novos recursos no Controller via REST

--- src/main/java/com/fabio/ProductDetailController.java
+++ src/main/java/com/fabio/ProductDetailController.java
@@ -1,55 +1,111 @@
-package com.fabio;		
- 		
- import javax.validation.Valid;
+package com.fabio;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.MutablePropertyValues;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.DataBinder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.RequestBody;		
- import org.springframework.web.bind.annotation.RequestMapping;		
- import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;		
- 		
- @RestController		
- @RequestMapping("/products")		
- public class ProductDetailController {		
- 		
- 	private final ProductDetailRepository repository;		
- 	private final ProductDetailValidator validator;		
- 			
- 	@Autowired		
- 	public ProductDetailController(ProductDetailRepository repository, ProductDetailValidator validator){		
- 		this.repository = repository;		
- 		this.validator = validator;
- 	}		
- 	
- 	@InitBinder
- 	protected void initBinder(WebDataBinder binder){
- 		binder.addValidators(validator);
- 	}
- 	
- 	@RequestMapping(method = RequestMethod.GET)		
- 	public Iterable findAll(@RequestParam(value="page", defaultValue="0", required=false) int page,
- 			@RequestParam(value="count", defaultValue="3", required=false) int count,
- 			@RequestParam(value="order", defaultValue="ASC", required=false) Sort.Direction direction,
- 			@RequestParam(value="sort", defaultValue="productName", required=false) String sortProperty){		
- 		System.out.println("=== findAll() ===");
- 		Page result = repository.findAll(new PageRequest(page, count, new Sort(direction, sortProperty)));
- 		return result.getContent();
- 	}		
- 			
- 	/**		
- 	 * @RequestBody: para fazer o biding pelo spring		
- 	 * e preencher automaticamente o objeto através de dados no formato JSON		
- 	 */		
- 	@RequestMapping(method = RequestMethod.POST)		
- 	public ProductDetail create(@RequestBody @Valid ProductDetail detail){		
- 		System.out.println("=== Save() ===");		
- 		return repository.save(detail);		
- 	}		
- 			
- }
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@RequestMapping("/products")
+public class ProductDetailController {
+
+	private final ProductDetailRepository repository;
+	private final ProductDetailValidator validator;
+	private final ObjectMapper objectMapper;
+
+	@Autowired
+	public ProductDetailController(ProductDetailRepository repository, ProductDetailValidator validator,
+			ObjectMapper objectMapper) {
+		this.repository = repository;
+		this.validator = validator;
+		this.objectMapper = objectMapper;
+	}
+
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		binder.addValidators(validator);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Iterable findAll(@RequestParam(value = "page", defaultValue = "0", required = false) int page,
+			@RequestParam(value = "count", defaultValue = "3", required = false) int count,
+			@RequestParam(value = "order", defaultValue = "ASC", required = false) Sort.Direction direction,
+			@RequestParam(value = "sort", defaultValue = "productName", required = false) String sortProperty) {
+		Page result = repository.findAll(new PageRequest(page, count, new Sort(direction, sortProperty)));
+		return result.getContent();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ProductDetail find(@PathVariable String id) {
+		ProductDetail detail = repository.findOne(id);
+		if (detail == null) {
+			throw new ProductNotFoundException();
+		} else {
+			return detail;
+		}
+	}
+
+	/**
+	 * @RequestBody: para fazer o biding pelo spring e preencher automaticamente
+	 *               o objeto através de dados no formato JSON
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public ProductDetail create(@RequestBody @Valid ProductDetail detail) {
+		return repository.save(detail);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public HttpEntity update(@PathVariable String id, HttpServletRequest request) throws IOException {
+		ProductDetail existing = repository.findOne(id);
+		ProductDetail updated = objectMapper.readerForUpdating(existing).readValue(request.getReader());
+		repository.save(updated);
+		MutablePropertyValues propertyValues = new MutablePropertyValues();
+		propertyValues.add("productId", updated.getProductId());
+		propertyValues.add("productName", updated.getProductName());
+		propertyValues.add("shortDescription", updated.getShortDescription());
+		propertyValues.add("LongDescription", updated.getLongDescription());
+		propertyValues.add("inventoryId", updated.getInventoryId());
+		DataBinder binder = new DataBinder(updated);
+		binder.addValidators(validator);
+		binder.bind(propertyValues);
+		binder.validate();
+		if (binder.getBindingResult().hasErrors()) {
+			return new ResponseEntity<>(binder.getBindingResult().getAllErrors(), HttpStatus.BAD_REQUEST);
+		} else {
+			return new ResponseEntity<>(updated, HttpStatus.ACCEPTED);
+		}
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public HttpEntity delete(@PathVariable String id) {
+		ProductDetail detail = find(id);
+		repository.delete(id);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+
+	@ResponseStatus
+	static class ProductNotFoundException extends RuntimeException {
+	}
+
+}
